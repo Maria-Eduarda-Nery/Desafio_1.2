c81b5db99dd64af93a281b67752f55f35d925400
Maria-Eduarda-Nery <142039771+Maria-Eduarda-Nery@users.noreply.github.com>
2024-02-28 16:48:55 -0300

    acabando

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Mostra na tela a mensagem "Ano novo está chegando!!", pelo console.log;
 console.log("Ano novo está chegando");
 
-//variavel que receba o nome da cantora 
+//variavel que recebe o nome da cantora 
 let cantora = "Mariah Carey";
 console.log(cantora);
 
@@ -10,8 +10,8 @@ let idade = 54;
 
 console.log(idade);
 
-//variável do ano atual - a idade 
-let ano = 2023;
+//variavel do ano atual - a idade 
+let ano = 2024;
 let _idade = 54;
 
 console.log((ano = ano - _idade));
@@ -23,9 +23,9 @@ console.log(quartaFeira);
 console.log(typeof quartaFeira);
 
 //variavel para idade adequada
-let Idade = 27;
+let Idade = 20;
 
-if (Idade >= 18) {
+if (Idade >= 15) {
   console.log(
     "Voce esta na idade certa, vai poder curtir o show "
   );
@@ -38,12 +38,12 @@ let mes = "Agosto";
 let $mes = "Dezembro";
 let _mes = `Junho`;
 
-if (mes != "Agosto" || $mes == "Dezembro" || _mes != `Junho`) {
+if (mes != "Agosto" || $mes == "Dezembro" || _mes != "Junho") {
   console.log(
-    `Uma ou mais opções estão corretas. O mês escolhido foi o mês de ${$mes}`
+    `uma ou mais opçõs estão corretas, O mes escolhido foi ${$mes}`
   );
 } else {
   console.log(
-    `Algo de errado não está certo, o mês digitado foi o mês ${Junho}`
+    `algo não esta correto, o mes digitado foi ${Junho}`
   );
 }
